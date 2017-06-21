518d8625dde7c4388efcc6a305b0878055787853
aalePotter <floresb.alejandra@gmail.com>
2017-06-20 21:49:16 -0700

    bLL

---
+++ Assistance_ControlBLL/TablesClasses/tcEmpleadoPermiso.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assistance_ControlBLL.TablesClasses
+{
+    public class tcEmpleadoPermiso
+    {
+        public void Insertar(EmpleadoPermiso entEmpleadoPermiso)
+        {
+            try
+            {
+                using (AssistanceControlEntities entidad = new AssistanceControlEntities())
+                {
+                    entidad.EmpleadoPermisoes.Add(entEmpleadoPermiso);
+                    entidad.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                throw new Exception("Error al insertar empleadoPermiso.");
+            }
+        }
+        public void Eliminar(EmpleadoPermiso entEmpleadoPermiso)
+        {
+            try
+            {
+                List<EmpleadoPermiso> empleadosPermisoes = null;
+                using (AssistanceControlEntities entidad = new AssistanceControlEntities())
+                {
+                    var consulta = from c in entidad.EmpleadoPermisoes
+                                   where c.EmpleadoId == entEmpleadoPermiso.EmpleadoId
+                                   where c.PermisoId == entEmpleadoPermiso.PermisoId
+                                   select c;
+                    empleadosPermisoes = consulta.ToList();
+                    if (empleadosPermisoes.Count > 0)
+                    {
+                        foreach (EmpleadoPermiso empleadoPermiso in empleadosPermisoes)
+                        {
+                            //Cambiar estatus
+                        }
+                    }
+                    entidad.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                throw new Exception("Error al eliminar EmpleadoPermiso.");
+            }
+        }
+        public void Actualizar(EmpleadoPermiso entEmpleadoPermiso)
+        {
+            try
+            {
+                List<EmpleadoPermiso> empleadosPermisoes = null;
+                using (AssistanceControlEntities entidad = new AssistanceControlEntities())
+                {
+                    var consulta = from c in entidad.EmpleadoPermisoes
+                                   where c.EmpleadoId == entEmpleadoPermiso.EmpleadoId
+                                   where c.PermisoId == entEmpleadoPermiso.PermisoId
+                                   select c;
+                    empleadosPermisoes = consulta.ToList();
+                    if (empleadosPermisoes.Count > 0)
+                    {
+                        foreach (EmpleadoPermiso empleadoPermiso in empleadosPermisoes)
+                        {
+                            //
+                        }
+                    }
+                    entidad.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                throw new Exception("Error al actualizar empleadoPermiso.");
+            }
+        }
+    }
+}
---
+++ Assistance_ControlBLL/TablesClasses/tcPermiso.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assistance_ControlBLL.TablesClasses
+{
+    public class tcPermiso
+    {
+        public void Insertar(Permiso entPermiso)
+        {
+            try
+            {
+                using (AssistanceControlEntities entidad = new AssistanceControlEntities())
+                {
+                    entidad.Permisoes.Add(entPermiso);
+                    entidad.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                throw new Exception("Error al insertar Permiso.");
+            }
+        }
+        public void Eliminar(Permiso entPermiso)
+        {
+            try
+            {
+                List<Permiso> permisos = null;
+                using (AssistanceControlEntities entidad = new AssistanceControlEntities())
+                {
+                    var consulta = from c in entidad.Permisoes
+                                   where c.PermisoId == entPermiso.PermisoId
+                                   select c;
+                    permisos = consulta.ToList();
+                    if (permisos.Count > 0)
+                    {
+                        foreach (Permiso permiso in permisos)
+                        {
+                            //Cambiar estatus
+                        }
+                    }
+                    entidad.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                throw new Exception("Error al eliminar Permiso.");
+            }
+        }
+        public void Actualizar(Permiso entPermiso)
+        {
+            try
+            {
+                List<Permiso> Permisos = null;
+                using (AssistanceControlEntities entidad = new AssistanceControlEntities())
+                {
+                    var consulta = from c in entidad.Permisoes
+                                   where c.PermisoId == entPermiso.PermisoId
+                                   select c;
+                    Permisos = consulta.ToList();
+                    if (Permisos.Count > 0)
+                    {
+                        foreach (Permiso permiso in Permisos)
+                        {
+                            //
+                        }
+                    }
+                    entidad.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                throw new Exception("Error al actualizar Permiso.");
+            }
+        }
+    }
+}
